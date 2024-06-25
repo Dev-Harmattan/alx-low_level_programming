b3a3be5e6d7456e51744bfab337d63120860ee8e
Sodiq Ganiyu <ganiyusodiq059@gmail.com>
2024-06-25 13:04:26 -0700

    print all alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point of the function
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+
+}
