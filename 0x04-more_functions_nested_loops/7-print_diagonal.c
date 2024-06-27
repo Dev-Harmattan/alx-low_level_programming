26f9997b4f8be5b0935340d11db3c22919d29685
Sodiq Ganiyu <ganiyusodiq059@gmail.com>
2024-06-27 10:56:28 -0700

    0x04-more_functions_nested_loops task 8

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,7 +18,8 @@ void print_diagonal(int n)
 			for (j = 0; j < n; j++)
 			{
 				if (j == i)
-					_putchar('\\$');
+					_putchar('\\');
+					_putchar('$');
 				else if (j < i)
 					_putchar(' ');
 			}
